5328eac3916101b0d6d2c84f5f1408b27d60fbc1
Aria Dewangga <ariaintrovert@gmail.com>
2025-09-24 09:17:20 +0700

    PERTEMUAN 1
    ALGOTRIMA DAN STRUKTUR DATA

---
+++ Hello.py
@@ -0,0 +1,7 @@
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print ("hello Nama Aria Dewangga")
+print ("Hobi saya, mendaki, jogging, memancing, dan bermain game")
+print ("saya ingin menjadi seorang programmer")
+
